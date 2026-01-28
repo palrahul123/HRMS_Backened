9e6ea307f29703b6f8bf869313464a62e134cd22
Rahul123 <pr@unstoppablesoftwares.com>
2026-01-28 07:07:06 +0530

    Added a Middleware

---
+++ Core.Application/Common/APIResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Application.Common
+{
+    public class APIResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+        public APIResponse() { }
+
+        public APIResponse(bool success, string message, object data = null)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+    }
+}
--- HRMS/Middlewares/ExceptionMiddleware.cs
+++ HRMS/Middlewares/ExceptionMiddleware.cs
@@ -1,13 +1,20 @@
-﻿namespace HRMS.Middlewares
+﻿using Core.Application.Common;
+using System.Net;
+using System.Text.Json;
+
+namespace HRMS.Middlewares
 {
     // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
 
-        public ExceptionMiddleware(RequestDelegate next)
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
+
         }
 
         public async Task Invoke(HttpContext httpContext)
@@ -16,14 +23,31 @@
             {
                 await _next(httpContext);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                _logger.LogError(ex, "An unhandled exception occurred.");
+                await HandleExceptionAsync(httpContext, ex);
             }
 
         }
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new APIResponse
+            {
+                Success = false,
+                Message = exception.Message,
+                Data = null
+            };
+
+            var json = JsonSerializer.Serialize(response);
+            return context.Response.WriteAsync(json);
+        }
     }
+
+
 
     // Extension method used to add the middleware to the HTTP request pipeline.
     public static class ExceptionMiddlewareExtensions
