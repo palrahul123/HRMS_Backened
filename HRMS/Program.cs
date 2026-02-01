5c88a548928db6ec2ef1e6e3014a6e3560dfd271
Rahul123 <pr@unstoppablesoftwares.com>
2026-02-01 16:12:05 +0530

    error is coming when adding swashbuckle

--- HRMS/Program.cs
+++ HRMS/Program.cs
@@ -1,15 +1,14 @@
 using Core.Application;
 using Infrastructure.Persistence.Extensions;
-using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi(options =>
+builder.Services.AddSwaggerGen(options =>
 {
-    options.OpenApiVersion = OpenApiSpecVersion.OpenApi3_0;
+    options.SwaggerDoc("v1", new() { Title = "My API", Version = "v1" });
 });
 
 builder.Services.AddApplicationServices(builder.Configuration);
@@ -18,7 +17,12 @@ builder.Services.AddPersistenceData(builder.Configuration);
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi("/openapi/{documentName}.yaml");
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+
+    });
 
 }
 
@@ -27,8 +31,3 @@ app.UseHttpsRedirection();
 
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
